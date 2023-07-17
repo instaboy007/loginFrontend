986a47a4d87a701a24da948856bea9dba23a4cd7
unknown <19PW21@cs2k16.amcs.ws2k12.psgcs.edu>
2023-07-17 12:05:02 +0530

    Added Register Page

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/login')
 def index():
     return render_template("auth/login.html")
+
+@app.route('/register')
+def registerUser():
+    return render_template("auth/register.html")
+
